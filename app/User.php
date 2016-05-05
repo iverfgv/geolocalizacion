6dc8ba4e9306bd51d90b161c3b13e71fa46e9f5a
iver gonzalez vidal <iver.fabi13@gmail.com>
2016-05-05 09:52:14 -0500

    mensaje usuarios

--- app/User.php
+++ app/User.php
@@ -7,9 +7,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
   public $timestamps = false;
-   protected $table='usuarios';
-   protected $fillable=['usuario','nombre','contrasena','perfiles_id','ubicaciones_id','activo'];
-   protected $guarded=['id'];
+  protected $table='usuarios';
+  protected $fillable=['usuario','nombre','contrasena','perfiles_id','ubicaciones_id','activo'];
+  protected $guarded=['id'];
 
     /**
      * The attributes that should be hidden for arrays.
