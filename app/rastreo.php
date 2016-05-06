b03bba219a5661795174b23851326cf53e5658e2
Herrera Lopez Jose Raul <raulithoherrera@gmail.com>
2016-05-06 09:42:21 -0500

    Actualizar eliminar

--- app/rastreo.php
+++ app/rastreo.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class rastreo extends Model
 {
    public $timestamps = false;
-   protected $table='rastreo';
-   protected $fillable=['embarques_id','fecha','firma','entrada','usuarios_id','ubicaciones_id','placas'];
+   protected $table='rastreos';
+   protected $fillable=['embarques_id','fecha','firma','entrada','usuario_id','ubicaciones_id','placas'];
    protected $guarded=['id'];
 }
